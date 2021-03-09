d183ea9fd27867f84d93c56680edefaf3a540528
Matthew Wessel <nucmatt@gmail.com>
2021-03-08 19:56:39 -0600

    Build: Create AddTransaction component
    Creates the AddTransaction component and brings it into App.js.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpense from './components/IncomeExpense';
 import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Balance />
         <IncomeExpense />
         <TransactionList />
+        <AddTransaction />
 			</div>
 		</div>
 	);
