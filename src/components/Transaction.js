43c69add1cde7ed4b5879a3813780b88266766f3
Matthew Wessel <nucmatt@gmail.com>
2021-03-10 21:21:26 -0600

    Build: Create Transaction component
    Creates the Transaction component and brings it in to TransactionList.

---
+++ src/components/Transaction.js
@@ -0,0 +1,11 @@
+export const Transaction = ({ transaction }) => {
+	return (
+		<li className='minus'>
+			{transaction.text} <span>-$400</span>
+			<button className='delete-btn'>x</button>
+		</li>
+	);
+};
+
+
+
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { Transaction } from './Transaction';
+
 import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
@@ -8,10 +10,7 @@ const TransactionList = () => {
 			<h3>History</h3>
 			<ul className='list'>
 				{transactions.map((transaction) => (
-					<li className='minus'>
-						{transaction.text} <span>-$400</span>
-						<button className='delete-btn'>x</button>
-					</li>
+					<Transaction key={transaction.id} transaction={transaction} />
 				))}
 			</ul>
 		</>
