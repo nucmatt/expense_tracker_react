77aced2acc41e460254e62f227a723646e3e61ff
Matthew Wessel <nucmatt@gmail.com>
2021-03-16 15:33:17 -0500

    Cleanup: Remove dummy transactions from initialState
    Removes the dummy transactions added to initialState to start the
    project.

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from './AppReducer';
 
 // Initial state
 const initialState = {
-	transactions: [
-		{ id: 1, text: 'Flower', amount: -20 },
-		{ id: 2, text: 'Salary', amount: 300 },
-		{ id: 3, text: 'Book', amount: -10 },
-		{ id: 4, text: 'Camera', amount: 150 },
-	],
+	transactions: [],
 };
 
 // Create context
@@ -34,7 +29,11 @@ export const GlobalProvider = ({ children }) => {
 
 	return (
 		<GlobalContext.Provider
-			value={{ transactions: state.transactions, deleteTransaction, addTransaction }}
+			value={{
+				transactions: state.transactions,
+				deleteTransaction,
+				addTransaction,
+			}}
 		>
 			{children}
 		</GlobalContext.Provider>
