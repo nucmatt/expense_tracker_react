97a79215d25c7eee89643906107b77109bb896a7
Matthew Wessel <nucmatt@gmail.com>
2021-05-07 08:55:20 -0500

    Build: Create controllers folder and transactioncontroller.js
    Creates the controllers folder and adds transactioncontroller.js to
    separate concerns into the MVC architecture. server.js is modified to
    to now use transactioncontroller.js.

---
+++ controllers/transactioncontroller.js
@@ -0,0 +1,6 @@
+// @desc Get all transactions
+// @route GET/api/v1/transactions
+// @access Public
+export function getTransactions(req, res, next) {
+	res.send('GET transactions');
+}
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 const router = Router();
-
-router.get('/', (req, res) => res.send('Hello'));
+import { getTransactions } from '../controllers/transactioncontroller';
+// Here the '/' refers to whatever route this export connects to in server.js in the app.use statement uses this file. (/api/v1/transactions in this case)
+// router.get('/', (req, res) => res.send('Hello'));
+router.route('/').get(getTransactions);
 
 export default router;
