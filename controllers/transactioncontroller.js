1d33633f21b04fb86fc027788b66750531cc64e5
Matthew Wessel <nucmatt@gmail.com>
2021-05-12 09:37:40 -0500

    Build: Imports Transaction model to transactioncontroller
    Imports the Transaction model into the transactioncontroller to build
    the routes out. transactioncontroller functions are converted to arrow
    functions to more clearly utilize async/await terminology to deal with
    promises returned.

--- controllers/transactioncontroller.js
+++ controllers/transactioncontroller.js
@@ -1,20 +1,22 @@
+import Transaction from '../models/Transaction.js';
+
 // @desc Get all transactions
 // @route GET/api/v1/transactions
 // @access Public
-export function getTransactions(req, res, next) {
+export const getTransactions = async (req, res, next) => {
 	res.send('GET transactions');
 }
 
 // @desc Add transaction
 // @route POST /api/v1/transactions
 // @access Public
-export function addTransaction(req, res, next) {
+export const addTransaction = async (req, res, next) => {
 	res.send('POST transaction');
 }
 
 // @desc Delete transaction
 // @route DELETE /api/v1/transactions/:id
 // @access Public
-export function deleteTransaction(req, res, next) {
+export const deleteTransaction = async (req, res, next) => {
 	res.send('DELETE transaction');
 }
