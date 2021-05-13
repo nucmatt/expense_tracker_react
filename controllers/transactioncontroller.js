ba388a1f2176426bb843d9e4933898503f4edbfd
Matthew Wessel <nucmatt@gmail.com>
2021-05-13 08:51:57 -0500

    Build: Add getTransactions response from database
    Adds async/await and try/catch to create a fully functional
    getTransactions route from the database.

--- controllers/transactioncontroller.js
+++ controllers/transactioncontroller.js
@@ -4,19 +4,33 @@ import Transaction from '../models/Transaction.js';
 // @route GET/api/v1/transactions
 // @access Public
 export const getTransactions = async (req, res, next) => {
-	res.send('GET transactions');
-}
+	// res.send('GET transactions');
+	try {
+		const transactions = await Transaction.find();
+
+		return res.status(200).json({
+			success: true,
+			count: transactions.length,
+			data: transactions,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			error: 'Server Error',
+		});
+	}
+};
 
 // @desc Add transaction
 // @route POST /api/v1/transactions
 // @access Public
 export const addTransaction = async (req, res, next) => {
 	res.send('POST transaction');
-}
+};
 
 // @desc Delete transaction
 // @route DELETE /api/v1/transactions/:id
 // @access Public
 export const deleteTransaction = async (req, res, next) => {
 	res.send('DELETE transaction');
-}
+};
