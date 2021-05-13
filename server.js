859c09758e03b553193f10151cad79ec506b5243
Matthew Wessel <nucmatt@gmail.com>
2021-05-13 11:02:55 -0500

    Chore: Initialize morgan in server.js
    Initializes morgan middleware in server.js to log all server requests
    when in development mode.

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const app = express();
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
 // app.get('/', (req, res) => res.send('Hello'));
 // This app.use statement mounts the transactions route brought in above.
 app.use('/api/v1/transactions', transactions);
