e794984b6ab800e998c6d30a0098b8fc255a4add
Matthew Wessel <nucmatt@gmail.com>
2021-05-13 10:38:50 -0500

    Build: Add GET_TRANSACTION and TRANSACTION_ERROR cases
    Adds the GET_TRANSACTION and TRANSACTION_ERROR cases to the AppReducer
    and those same actions to the GlobalState context provider.

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,9 +1,12 @@
 import { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
+import axios from 'axios';
 
 // Initial state
 const initialState = {
 	transactions: [],
+	error: null,
+	loading: true
 };
 
 // Create context
@@ -14,6 +17,24 @@ export const GlobalProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(AppReducer, initialState);
 
 	// Actions
+	async function getTransactions() {
+		try {
+			// Remember, you don't need http://localhost:5000 in the request since it was added as a proxy in the client package.json
+			const res = await axios.get('/api/v1/transactions');
+
+			dispatch({
+				type: 'GET_TRANSACTIONS',
+				// The get request above returns the full body of the database transactions. Since we only want to the display the transactions in History we will access those in the data property of the response object, hence res.data.data.
+				payload: res.data.data,
+			});
+		} catch (error) {
+			dispatch({
+				type: 'TRANSACTION_ERROR',
+				payload: error.response.data.error
+			})
+		}
+	}
+
 	function deleteTransaction(id) {
 		dispatch({
 			type: 'DELETE_TRANSACTION',
